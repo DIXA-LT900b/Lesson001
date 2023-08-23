0831333135ebcc8757c9c00e751eebdb56a0210e
DIXA <dixa@xaker.ru>
2023-08-23 16:06:52 +0300

    Домашнее задание к занятию 4.1 Коллекции List Задача 1. Записная книга

--- src/TaskList.java
+++ src/TaskList.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 
 public class TaskList{
     private byte choose;
-    Scanner chooser;
-    Scanner tasker;
+    Scanner chooser = new Scanner(System.in);
+    Scanner tasker = new Scanner(System.in);
 
     TaskList (){
         ArrayList<String> tasklist = new ArrayList();
 
 
         while (true) {
-            choose = showmenu();
+            choose = showMenu();
 
             switch (choose){
                 case 1: {
                     System.out.println("Введите задачу для планирования:");
-                    tasker = new Scanner(System.in);
+
                     String task = tasker.nextLine();
                     tasklist.add(task);
                     System.out.println("Задача добавлена.");
@@ -24,19 +24,14 @@ public class TaskList{
                     break;
                 }
                 case 2: {
-                    System.out.println("Список задач:");
-                    for (int i = 0; i<tasklist.size(); i++){
-                        System.out.println( (i + 1) + ". " + tasklist.get(i).toString() );
-                    }
-                    System.out.println();
+                    showTasks(tasklist);
+
 
                     break;
                 }
                 case 3: {
-                    System.out.println("Список задач:");
-                    for (int i = 0; i<tasklist.size(); i++){
-                        System.out.println( (i + 1) + ". " + tasklist.get(i).toString() );
-                    }
+                    showTasks(tasklist);
+
                     System.out.println("Введите номер задачи, которую необходимо удалить:");
                     int taskNum = tasker.nextByte();
                     tasklist.remove(taskNum - 1);
@@ -54,7 +49,7 @@ public class TaskList{
             }
         }
     }
-    private byte showmenu(){
+    private byte showMenu(){
         System.out.println("Выберите действие:");
         System.out.println("1. Добавить задачу");
         System.out.println("2. Вывести список задач");
@@ -62,10 +57,14 @@ public class TaskList{
         System.out.println("0. Выход");
         System.out.println();
 
-        chooser = new Scanner(System.in);
-
-
         return chooser.nextByte();
 
+    }
+    private void showTasks(ArrayList tasklist){
+        System.out.println("Список задач:");
+        for (int i = 0; i<tasklist.size(); i++){
+            System.out.println( (i + 1) + ". " + tasklist.get(i).toString() );
+        }
+        System.out.println();
     }
 }
